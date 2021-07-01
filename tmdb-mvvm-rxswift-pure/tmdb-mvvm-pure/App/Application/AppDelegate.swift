a8eb0f2483de49b8fc139cf48c2063ac5c234af0
Mateusz Nicpoń <mnicpon@pgs-soft.com>
2021-07-01 15:14:41 +0200

    Disable animations.

--- tmdb-mvvm-rxswift-pure/tmdb-mvvm-pure/App/Application/AppDelegate.swift
+++ tmdb-mvvm-rxswift-pure/tmdb-mvvm-pure/App/Application/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        UIView.setAnimationsEnabled(false)
         window = UIWindow(frame: UIScreen.main.bounds)
         
         if let window = window {
